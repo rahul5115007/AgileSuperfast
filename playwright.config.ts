a3fb882536c9e1cced585600f3a840d3693afa97
rahul joshi <rahuljoshi5611742@gmail.com>
2026-01-12 11:21:11 +0530

    Initial cehckup

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,79 +1,67 @@
-import { defineConfig, devices } from '@playwright/test';
+import { PlaywrightTestConfig} from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
-export default defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+import Browser from "./src/framework/manager/Browser";
+
+dotenv.config();
+
+const timeInMin: number = 60 * 1000;
+const config: PlaywrightTestConfig = {
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    browserName: Browser.type(process.env.BROWSER.toLowerCase()),
+    headless: false,
+    channel: Browser.channel(process.env.BROWSER.toLowerCase()),
+    launchOptions: {
+      args: ["--start-maximized", "--disable-extensions", "--disable-plugins"],
+      headless: false,
+      timeout: Number.parseInt(process.env.BROWSER_LAUNCH_TIMEOUT, 10),
+      slowMo: 100,
+      downloadsPath: "./test-results/downloads",
+    },
+    viewport: null,
+    ignoreHTTPSErrors: true,
+    acceptDownloads: true,
+    actionTimeout: Number.parseInt(process.env.ACTION_TIMEOUT, 10) * timeInMin,
+    navigationTimeout: Number.parseInt(process.env.NAVIGATION_TIMEOUT, 10) * timeInMin,
+    screenshot: { 
+      mode: "only-on-failure",
+      fullPage: true,
+    },
+    video: "retain-on-failure",
   },
-
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+  testDir: "./src/tests",
+  outputDir: "./test-results/failure",
+  retries: Number.parseInt(process.env.RETRIES, 10),
+  preserveOutput: "failures-only",
+  reportSlowTests: null,
+  timeout: Number.parseInt(process.env.TEST_TIMEOUT, 10) * timeInMin,
+  workers: Number.parseInt(process.env.PARALLEL_THREAD, 10),
+  reporter: [
+    ["dot"],
+    ["allure-playwright", {
+      detail: false,
+      suiteTitle: false,
+      environmentInfo: {
+        OS: process.platform.toUpperCase(),
+        BROWSER: process.env.BROWSER.toUpperCase(),
+        BASE_URL: process.env.BASE_URL,
+      },
+    }],
+    ['html', { open: 'never', outputFolder: "./test-results/report" }],
+    ["junit", { outputFile: "./test-results/results/results.xml" }],
+    ["json", { outputFile: "./test-results/results/results.json" }],
+    ["./src/framework/logger/TestListener.ts"],
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-});
+  projects: [  
+    {
+      name: "local",
+      testMatch: `*${process.env.TEST_NAME.trim()}*`,
+    },
+    {
+      name: "suite",
+      testMatch: "*.test.ts",
+    },
+  ],
+};
+export default config;
