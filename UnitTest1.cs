e33eaa63fe5a35d5844be666a8e897352bdb0943
rahul joshi <rahuljoshi5611742@gmail.com>
2026-07-31 12:38:27 +0530

    initial code

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit;
+
+namespace PlaywrightTests;
+
+public class UnitTest1: PageTest
+{
+    [Fact]
+    public async Task HasTitle(){
+        await Page.GotoAsync("http://localhost:5000");
+
+        //Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Your store."));
+
+    }
+    
+}
